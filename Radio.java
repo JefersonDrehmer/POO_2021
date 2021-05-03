2e6cb63ce16256b1cc0864a03b879bd4cec6a60a
JefersonDrehmer <73262758+JefersonDrehmer@users.noreply.github.com>
2021-05-03 16:23:47 -0300

    Add files via upload

---
+++ Radio.java
@@ -0,0 +1,21 @@
+/*
+ 
+ */
+package radio;
+
+
+public class Radio {
+
+   
+    public static void main(String[] args) {
+       aula1 sc = new aula1();
+       sc.setModelo("Pioneer");
+       sc.setCorled("Vermelho");
+       sc.setVolume(40);
+       sc.setLigado(true);
+       sc.Status();
+       sc.getVolume();
+       
+    }
+    
+}
